ccd425a4074adbc92403285f3d7c2af0319b4140
Daniil <ventsovdaniil@yandex.ru>
2020-07-31 17:29:53 +0300

    reforge menu, i will definily survive...

--- cardproduct.js
+++ cardproduct.js
@@ -1,8 +1,10 @@
+/* MENU */
+
+
+//меню
 {
-  //меню
-  {
-    const menu = document.createElement('div');
-    menu.innerHTML = `
+  const menu = document.createElement('div');
+  menu.innerHTML = `
   <div class="menu_and_shopping_basket_layout">
   <div class="menu_type">
       <ul class="list_menu" >
@@ -26,41 +28,42 @@
           </li>
       </ul>
   </div>`;
-    document.querySelector('.menu_type_js').appendChild(menu);
-  }
+  document.querySelector('.menu_type_js').appendChild(menu);
+}
 
-  // рендер по категории
-  const renderByCategory = (category) => {
-    const result = cards.filter(item => item.category === category);
-    render(result);
-  }
+// рендер по категории
+const renderByCategory = (category) => {
+  const result = cards.filter(item => item.category === category);
+  render(result);
+}
 
-  const cards = data.menu;
-  const defaultCategory = "sandwiches";
+const cards = data.menu;
+const defaultCategory = "sandwiches";
 
-  renderByCategory(defaultCategory);
+renderByCategory(defaultCategory);
 
-  // смена класса по клику
-  const btn = $('.target');
-  btn.on('click', function () {
+// смена класса по клику
+const btn = $('.target');
+btn.on('click', function () {
 
-    renderByCategory($(this).data('category'));
+  renderByCategory($(this).data('category'));
 
-    $(".target").removeClass('active');
-    $(this).addClass('active');
+  $(".target").removeClass('active');
+  $(this).addClass('active');
 
-  });
+});
 
+/*PRODUCT LIST*/
 
-  // рендер продуктовых карточек 
-  function render(cards) {
-    document.querySelector('.cards_wrapper').innerHTML = '';
+// рендер продуктовых карточек 
+function render(cards) {
+  document.querySelector('.cards_wrapper').innerHTML = '';
 
-    for (let i = 0; i < cards.length; i++) {
-      const new_card = document.createElement('div');
+  for (let i = 0; i < cards.length; i++) {
+    const new_card = document.createElement('div');
 
-      new_card.classList.add('card_product');
-      new_card.innerHTML = `  
+    new_card.classList.add('card_product');
+    new_card.innerHTML = `  
       <div id="item_box">
         <div class="logo_market">
           <img src="${data.markets[cards[i].market]?.image}" alt="picture" style="">
@@ -89,39 +92,39 @@
         <button class="buy_basket" >В КОРЗИНУ</button>
       </div>`;
 
-      document.querySelector('.cards_wrapper').appendChild(new_card);
-    }
+    document.querySelector('.cards_wrapper').appendChild(new_card);
   }
-
-
-  // меню с выбором ингридиентов 
-  const favDialog = document.getElementById('favDialog');
-  $(document).on('click', '.description_border', function () {
-    favDialog.showModal();
-  })
-
-  $(document).on('click', '.closeButton', function () {
-    favDialog.close();
-  })
-
-  // колиство товара
-  $(document).on('click', 'button_minus_style', function () {
-    var $input = $(this).parent().find('input');
-    var count = parseInt($input.val()) - 1;
-    count = count < 1 ? 1 : count;
-    $input.val(count);
-    $input.change();
-    return false;
-  });
-
-  $('.button_plus_style').click(function () {
-    var $input = $(this).parent().find('input');
-    $input.val(parseInt($input.val()) + 1);
-    $input.change();
-    return false;
-  });
-
 }
+
+
+// меню с выбором ингридиентов 
+const favDialog = document.getElementById('favDialog');
+$(document).on('click', '.description_border', function () {
+  favDialog.showModal();
+})
+
+$(document).on('click', '.closeButton', function () {
+  favDialog.close();
+})
+
+// колиство товара
+$(document).on('click', 'button_minus_style', function () {
+  var $input = $(this).parent().find('input');
+  var count = parseInt($input.val()) - 1;
+  count = count < 1 ? 1 : count;
+  $input.val(count);
+  $input.change();
+  return false;
+});
+
+$('.button_plus_style').click(function () {
+  var $input = $(this).parent().find('input');
+  $input.val(parseInt($input.val()) + 1);
+  $input.change();
+  return false;
+});
+
+
 
 // корзина покупок
 const buy_basket = document.createElement('div');
@@ -253,21 +256,54 @@ document.querySelector('.buy_basket_js').appendChild(buy_basket);
 
     } else if (i === 2) {
 
-      commponentsProductPopUp.vegetable.push($(this).find(".card_ingredient_title").text());
-      $(this).addClass('active_card_ingredient_style');
+      let ingredient = $(this).find(".card_ingredient_title").text();
+      $(this).toggleClass('active_card_ingredient_style');
 
-      $(document).on('click', '.card_ingredient_style', function () {
-        $(this).removeClass('active_card_ingredient_style');
-        commponentsProductPopUp.vegetable.splice(this, 1);
-      });
+      const index = commponentsProductPopUp.vegetable.indexOf(ingredient);
+      if (index !== -1) {
+        commponentsProductPopUp.vegetable.splice(index, 1);
+      } else {
+        commponentsProductPopUp.vegetable.push(ingredient);
+      }
 
     } else if (i === 3) {
+      let ingredient = $(this).find(".card_ingredient_title").text();
+      const index = commponentsProductPopUp.sauce.indexOf(ingredient);
 
-      $(document).on('click', '.card_ingredient_style', function () {
-        quantitySauce++
-        commponentsProductPopUp.sauce.push($(this).find(".card_ingredient_title").text());
-        $(this).toggleClass('active_card_ingredient_style');
-      });
+      if (commponentsProductPopUp.sauce.length < 3) {
+        if (index !== -1) {
+          commponentsProductPopUp.sauce.splice(index, 1);
+        } else {
+          commponentsProductPopUp.sauce.push(ingredient);
+          $(this).addClass('active_card_ingredient_style');
+        }
+      } else {
+        if (index !== -1) {
+          commponentsProductPopUp.sauce.splice(index, 1);
+          $(this).removeClass('active_card_ingredient_style');
+        }
+      }
+
+
+      // $(this).toggleClass('active_card_ingredient_style');
+
+      // if (index !== -1) {
+      //   commponentsProductPopUp.sauce.splice(index, 1);
+      // } else {
+      //   commponentsProductPopUp.sauce.push(ingredient);
+      // }
+
+    } else if (i === 4) {
+
+
+      $(this).toggleClass('active_card_ingredient_style');
+
+      const index = commponentsProductPopUp.filling.indexOf(ingredient);
+      if (index !== -1) {
+        commponentsProductPopUp.filling.splice(index, 1);
+      } else {
+        commponentsProductPopUp.filling.push(ingredient);
+      }
 
     }
     console.log(commponentsProductPopUp);
